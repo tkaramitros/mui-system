1a6f6c340e0123361f121152ff6ede5bee807efc
teo <karamitrosteo@gmail.com>
2022-02-01 19:43:58 +0200

    Customised MUI components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { makeStyles } from "@mui/styles";
 import {
   AppBar,
   Toolbar,
@@ -10,29 +11,50 @@ import {
 import { NotificationsNone as NotificationsNoneIcon } from "@mui/icons-material";
 import { ChatBubbleOutline as ChatBubbleOutlineIcon } from "@mui/icons-material";
 import { PowerSettingsNew as PowerSettingsNewIcon } from "@mui/icons-material";
+import { Search as SearchIcon } from "@mui/icons-material";
+
+const useStyles = makeStyles({
+  searchInput: {
+    opacity: "0.6",
+    padding: "0px 8px",
+    fontSize: "0.8rem",
+    "&:hover": {
+      backgroundColor: "#f2f2f2",
+    },
+    "& .MuiSvgIcon-root": {
+      marginRight: "8px",
+    },
+  },
+});
 
 const Header = () => {
+  const classes = useStyles();
+
   return (
-    <AppBar position="static">
+    <AppBar position="static" style={{ backgroundColor: "#fff" }}>
       <Toolbar>
-        <Grid container>
+        <Grid container alignItems="center">
           <Grid item>
-            <InputBase />
+            <InputBase
+              placeholder="Search topics"
+              className={classes.searchInput}
+              startAdornment={<SearchIcon fontSize="small" />}
+            />
           </Grid>
           <Grid item sm></Grid>
           <Grid item>
             <IconButton>
               <Badge badgeContent={4} color="secondary">
-                <NotificationsNoneIcon />
+                <NotificationsNoneIcon fontSize="small" />
               </Badge>
             </IconButton>
             <IconButton>
               <Badge badgeContent={3} color="primary">
-                <ChatBubbleOutlineIcon />
+                <ChatBubbleOutlineIcon fontSize="small" />
               </Badge>
             </IconButton>
             <IconButton>
-              <PowerSettingsNewIcon />
+              <PowerSettingsNewIcon fontSize="small" />
             </IconButton>
           </Grid>
         </Grid>
