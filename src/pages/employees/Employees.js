40b74f8513024b5883daf6f9f11a4ab20524ba4b
teo <karamitrosteo@gmail.com>
2022-02-03 17:37:12 +0200

    Created DataTable and Pagination

--- src/pages/employees/Employees.js
+++ src/pages/employees/Employees.js
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import EmployeeForm from "./EmployeeForm";
 import PageHeader from "../../components/PageHeader";
 import { PeopleOutlineTwoTone } from "@mui/icons-material";
-import { Paper } from "@mui/material";
+import { Paper, TableBody, TableCell, TableRow } from "@mui/material";
 import { makeStyles } from "@mui/styles";
+import useTable from "../../components/useTable";
+import * as employeeService from "../../services/employeeService";
 
-const useStyle = makeStyles((theme) => ({
+const useStyles = makeStyles((theme) => ({
   pageContent: {
     margin: theme.spacing(5),
     padding: theme.spacing(3),
   },
 }));
 
+const headCells = [
+  { id: "fullName", label: "Employee Name" },
+  { id: "email", label: "Email Adress (Personal)" },
+  { id: "mobile", label: "Mobile Number" },
+  { id: "department", label: "Department", disableSorting: true },
+];
+
 const Employees = () => {
-  const classes = useStyle();
+  const [records, setRecords] = useState(employeeService.getAllEmployees());
+  const { TblContainer, TblHead, TblPagination, recordsAfterPaginAndSorting } =
+    useTable(records, headCells);
+
+  const classes = useStyles();
+
   return (
     <>
       <PageHeader
@@ -22,7 +36,21 @@ const Employees = () => {
         icon={<PeopleOutlineTwoTone fontSize="large" />}
       />
       <Paper className={classes.pageContent}>
-        <EmployeeForm />
+        {/*<EmployeeForm />*/}
+        <TblContainer>
+          <TblHead />
+          <TableBody>
+            {recordsAfterPaginAndSorting().map((item) => (
+              <TableRow key={item.id}>
+                <TableCell>{item.fullName}</TableCell>
+                <TableCell>{item.email}</TableCell>
+                <TableCell>{item.mobile}</TableCell>
+                <TableCell>{item.department}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </TblContainer>
+        <TblPagination />
       </Paper>
     </>
   );
